14044d9c811285c8899e0b58cab76645df285434
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-01-06 16:59:03 -0500

    Create AD - Reset User Password in Active Directory.ps1

---
+++ PowerShell/AD - Reset User Password in Active Directory.ps1
@@ -0,0 +1,18 @@
+﻿<# 
+.DESCRIPTION 
+	
+.NOTES 
+    File Name  : AD - Reset User Password in Active Directory
+    Author     : Bernardo Arocho
+.LINK 
+
+#> 
+Import-Module ActiveDirectory
+
+Get-Command -Module ActiveDirectory
+
+$Pass = ConvertTo-SecureString "!20btnhd24!" -AsPlainText -Force
+
+Set-ADAccountPassword -Identity bruce.wayne -NewPassword $pass –Reset
+
+Set-ADUser -Identity bruce.wayne -ChangePasswordAtLogon $true
