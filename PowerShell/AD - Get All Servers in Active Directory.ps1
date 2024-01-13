082a3fce279a8fe326895ef0b28a8b7437e741ba
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-01-13 18:52:10 -0500

    Create AD - Get All Servers in Active Directory.ps1

---
+++ PowerShell/AD - Get All Servers in Active Directory.ps1
@@ -0,0 +1,17 @@
+﻿<# 
+.DESCRIPTION 
+	
+.NOTES 
+    File Name  : AD - Get All Servers in Active Directory
+    Author     : Bernardo Arocho
+.LINK 
+
+#> 
+
+Import-Module ActiveDirectory
+
+Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address `
+| Sort-Object -Property Operatingsystem `
+| Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address '
+| Export-Csv C:\BTNHD_TEMP\server_dump.csv
