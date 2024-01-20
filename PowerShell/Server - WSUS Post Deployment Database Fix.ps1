0cc75b60819976726aca2fdff120fdb65254a4dd
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-01-20 17:26:33 -0500

    Server - WSUS Post Deployment Database Fix

---
+++ PowerShell/Server - WSUS Post Deployment Database Fix.ps1
@@ -0,0 +1,16 @@
+﻿<# 
+.DESCRIPTION 
+	
+.NOTES 
+    File Name  : 
+    Author     : Bernardo (BNTHD)
+.LINK 
+
+#> 
+Start-Process takeown.exe -ArgumentList '/f "C:\Program Files\Update Services\Database\VersionCheck.sql"' -Wait
+
+Start-Process icacls.exe -ArgumentList '"C:\Program Files\Update Services\Database\VersionCheck.sql" /grant "administrator:(F)"' -Wait
+
+(Get-Content "C:\Program Files\Update Services\Database\VersionCheck.sql") -replace "(^DECLARE @scriptMinorVersion\s+ int = \(11\)$)","DECLARE @scriptMinorVersion int = (51)" `
+| Set-Content "C:\Program Files\Update Services\Database\VersionCheck.sql"
+
