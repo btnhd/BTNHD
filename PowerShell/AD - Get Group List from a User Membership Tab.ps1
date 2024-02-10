836e9fdc4e0db620769ae15179efad850506a7ff
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-02-10 11:40:54 -0500

    Create AD - Get Group List from a User Membership Tab.ps1

---
+++ PowerShell/AD - Get Group List from a User Membership Tab.ps1
@@ -0,0 +1,25 @@
+﻿<# 
+.DESCRIPTION 
+	
+.NOTES 
+    File Name  : Get Group List from a User's Memebership Tab
+    Author     : Bernardo (BNTHD)
+.LINK 
+
+#> 
+
+# Import Active Directory Module into PowerShell
+Import-Module ActiveDirectory
+
+# Specify the username or user's DistinguishedName
+$userName = "Bruce.Wayne"
+
+# Get the user object
+$user = Get-ADUser -Identity $userName -Properties MemberOf
+
+# Display user's group memberships
+$UserData = Write-Host "Groups for $userName - "
+foreach ($group in $user.MemberOf) {
+    $groupName = (Get-ADGroup $group).Name
+    Write-Host "$groupName;"
+}
