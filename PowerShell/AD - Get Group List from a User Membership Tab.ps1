a7ad774d7cc33082c8b2d3236b9d5879f796c79e
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-02-17 13:19:23 -0500

    Update AD - Get Group List from a User Membership Tab.ps1
    updated version with options to export out the memberships into a CSV file.

--- PowerShell/AD - Get Group List from a User Membership Tab.ps1
+++ PowerShell/AD - Get Group List from a User Membership Tab.ps1
@@ -19,7 +19,13 @@ $user = Get-ADUser -Identity $userName -Properties MemberOf
 
 # Display user's group memberships
 $UserData = Write-Host "Groups for $userName - "
+
+# Use the below comment to export it out into a CSV file (make sure to change the file path to your location)
+#$UserData = Write-Output "Groups for $userName - " | Out-File -FilePath "C:\BTNHD_TEMP\$userName-GroupList.csv" -Append
 foreach ($group in $user.MemberOf) {
     $groupName = (Get-ADGroup $group).Name
     Write-Host "$groupName;"
+	
+	# Use the below comment to export it out into a CSV file (make sure to change the file path to your location)
+	#Write-Output "$groupName;" | Out-File -FilePath "C:\BTNHD_TEMP\$userName-GroupList.csv" -Append
 }
