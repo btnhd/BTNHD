53e771eae1ae8266047430f8b313c4dad6182cdc
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-03-02 19:52:02 -0500

    Create Server - Get LDAP Server from Domain.ps1
    TSR #062 - Get LDAP Server from Domain with PowerShell

---
+++ PowerShell/Server - Get LDAP Server from Domain.ps1
@@ -0,0 +1,29 @@
+﻿<# 
+.DESCRIPTION 
+	
+.NOTES 
+    File Name  : Get LDAP Server from Domain
+    Author     : Bernardo (BNTHD)
+.LINK 
+
+#> 
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Specify the domain you want to search in
+$domain = "btnhd.edu"
+
+# Get LDAP Server from the domain (quick and easy way to get the information needed)
+#Get-ADDomainController -Discover -DomainName $domain
+
+# Adding logic to discover the LDAP server for the domain and out putting it as text
+try {
+    $ldapServer = Get-ADDomainController -Discover -DomainName $domain
+    Write-Host "LDAP Server found:"
+    Write-Host "Server Name: $($ldapServer.Name)"
+    Write-Host "Server HostName: $($ldapServer.HostName)"
+	Write-Host "Server IP: $($ldapServer.IPv4Address)"
+    Write-Host "Server Site: $($ldapServer.Site)"
+} catch {
+    Write-Host "Error locating LDAP server: $_"
+}
