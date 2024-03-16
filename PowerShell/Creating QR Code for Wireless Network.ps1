46a5465b411eaddc74ebc6b418a4cc03a531710d
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-03-16 11:37:29 -0400

    Creating QR Code for Wireless Network
    The Server Room Episode #64

---
+++ PowerShell/Creating QR Code for Wireless Network.ps1
@@ -0,0 +1,23 @@
+﻿<# 
+.DESCRIPTION 
+	
+.NOTES 
+    File Name  : Creating QR Codes for Wireless Network
+    Author     : Bernardo (BNTHD)
+.LINK 
+
+#> 
+Install-Module -Name QRCodeGenerator
+
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+Import-Module QRCodeGenerator
+
+Get-Command -Module QRCodeGenerator
+
+New-QRCodeWifiAccess -SSID 'MYWIFI' -Password '123qwe' -Width 15 -OutPath 'C:\BTNHD_TEMP\WIFI.PNG'
+
+$WIRELESS_SSID = 'MYWIFI'
+$WIRELESS_PASSWORD = '123qwe'
+$DESTINATION_FILE = 'C:\BTNHD_TEMP\WIFI.PNG'
+New-QRCodeWifiAccess -SSID $WIRELESS_SSID -Password $WIRELESS_PASSWORD -Width 15 -OutPath $DESTINATION_FILE
