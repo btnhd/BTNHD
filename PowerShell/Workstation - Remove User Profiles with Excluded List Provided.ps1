73dcdb29b50ba9a3832f2470a17ca18405a8103a
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-03-30 21:59:11 -0400

    PowerShell: Remove User Profiles with Excluded List Provided – The Server Room #066
    PowerShell: Remove User Profiles with Excluded List Provided – The Server Room #066

---
+++ PowerShell/Workstation - Remove User Profiles with Excluded List Provided.ps1
@@ -0,0 +1,22 @@
+﻿# Define the computer name and the path to the user profiles
+$computerName = "PC02.btnhd.edu"
+$profilePath = "C:\Users"
+
+# List of profiles to exclude from deletion
+$profilesToExclude = @("bernardo.arocho", "btnhd", "Default", "Public")
+
+# Get a list of all user profiles on the remote computer
+$allProfiles = Get-WmiObject Win32_UserProfile -ComputerName $computerName | Where-Object { $_.Special -eq $false }
+
+foreach ($profile in $allProfiles) {
+    $userName = $profile.LocalPath.Split('\')[-1]
+
+    # Check if the current profile should be excluded
+    if ($profilesToExclude -notcontains $userName) {
+        # Delete the user profile
+        Write-Host "Deleting profile for $userName"
+        Remove-Item -Path "$profilePath\$userName" -Force -Recurse
+    } else {
+        Write-Host "Excluding profile for $userName from deletion"
+    }
+}
