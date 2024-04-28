a8d953a27e172cbe95bf86f4baae2e5024a630a8
Bernardo Arocho <30936611+btnhd@users.noreply.github.com>
2024-04-28 17:14:23 -0400

    Server - Output Files and Folders Names from Root Folder

---
+++ PowerShell/Server - Output Files and Folders Names from Root Folder.ps1
@@ -0,0 +1,15 @@
+﻿# Set the path to the root folder you want to analyze
+$rootFolder = "C:\Users\bernardo.arocho\Desktop\FILES"
+
+# Specify the output CSV file
+$outputFile = "$env:USERPROFILE\Downloads\File.csv"
+
+# Retrieve directory information and export to CSV
+Get-ChildItem -Path $rootFolder -File -Recurse | ForEach-Object {
+    [PSCustomObject]@{
+        FolderName = $_.Directory.Name
+        SubfolderName = $_.DirectoryName -replace [regex]::Escape($rootFolder), ""
+        FileName = $_.Name
+        FullPath = $_.FullName
+    }
+} | Export-Csv -Path $outputFile -NoTypeInformation
